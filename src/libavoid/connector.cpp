12d1189002ad1b3747034426d6527c13a0485716
valerioa <valerio@aimale.com>
2017-12-09 09:08:55 -0700

    added extras

--- src/libavoid/connector.cpp
+++ src/libavoid/connector.cpp
@@ -885,7 +885,7 @@ bool ConnRef::generatePath(void)
             {
                 // Check for consecutive points on opposite 
                 // corners of two touching shapes.
-                COLA_ASSERT(abs(i->pathNext->id.objID - i->id.objID) != 2);
+		COLA_ASSERT(abs((int)(i->pathNext->id.objID - i->id.objID) != 2));
             }
         }
     }
